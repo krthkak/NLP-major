f175edabaa78ba7d12b9d02c2e8ae10dc7dd142a
Karthik M Bharadwaj <krthkak@gmail.com>
2019-11-26 19:34:45 +0530

    changed fit_tansform to transform

--- appnlp.py
+++ appnlp.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import json,re
+from textblob import TextBlob
 
 app = Flask(__name__)
 PS = pickle.load(open('portStem.pkl', 'rb'))
@@ -15,7 +16,7 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
-def predict():
+def predicts():
     '''
     For rendering results on HTML GUI
     '''
@@ -23,17 +24,16 @@ def predict():
     out = request.form.get('review')
     
     review = re.sub('[^a-zA-Z]', ' ' ,out)
-    #return render_template('index.html', prediction_text='The Sentiment of the review is {}'.format(out))
+    review = review.lower()
     review = review.split()
     review = [PS.stem(word) for word in review if not word in set(stop_words)]
     review = " ".join(review)
     
-    corpus = [[review]]
-    x=CV.fit_transform(corpus).toarray()
+    corpus = [review]
+    x=CV.transform(corpus).toarray()
     pred = model.predict(x)
 
     output = senti[pred[0]]
-
     return render_template('index.html', prediction_text='The Sentiment of the review is {}'.format(output))
 
 
